33f5aa72f6acecb0c5f2422b71671c4f6247860e
Vladimir Bataller <vbatalle@everis.com>
2021-02-23 10:03:41 +0100

    es6 propagación

--- r00_EcmaScript6/E04_operador_propagacion.js
+++ r00_EcmaScript6/E04_operador_propagacion.js
@@ -4,4 +4,22 @@ function sumarCuatro(a,b,c,d){
 let datos = [2,5,3,6];
 //let resul = sumarCuatro(datos[0],datos[1],datos[2],datos[3]);
 let resul = sumarCuatro(...datos);
-console.log("La suma de ",...datos, "es", resul);
+console.log("La suma de",...datos, "es", resul);
+
+
+const a = [1,2,3];
+const b = [4,5,6];
+const c = [...a, ...b];
+console.log(c);
+
+const d = {x:10, y:20};
+const e = {y: 30, z:30}
+const f = {...d, ...e};
+console.log(f); 
+
+const {x,y} = f;// Desestruturaación de variables
+console.log('x', x);
+console.log('y', y);
+console.log('z', f.z);
+
+//Robert C Martin (Clean Code - Codigo Limpio)
