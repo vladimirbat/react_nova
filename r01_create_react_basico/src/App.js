899e78af2af41147f8051313bfa22e2dd7ef6022
Vladimir Bataller <vbatalle@everis.com>
2021-02-23 11:35:18 +0100

    create react app

--- r01_create_react_basico/src/App.js
+++ r01_create_react_basico/src/App.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+function App() {
+  const texto = 'Hola';
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
-    );
-  }
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Aprende react
+        </a>
+        <div>{texto}</div>
+      </header>
+    </div>
+  );
 }
 
 export default App;
